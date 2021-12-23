58e4d736c59dd6815e2dca5bdc1abe451b8947a4
Carl Wiede <carlwie@student.chalmers.se>
2021-12-23 11:32:20 +0100

    added the lists to the header file

--- commands.h
+++ commands.h
@@ -3,3 +3,7 @@
 int ush_cd(char **args);
 int ush_help(char **args);
 int ush_exit(char **args);
+
+// Declaration of function names and their pointers
+char *builtin_str[];
+int (*builtin_func[]) (char **);
